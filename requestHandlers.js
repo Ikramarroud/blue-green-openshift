e27de52265565e970c1ad0e750b128f34ac4869b
methena <118998724+methena@users.noreply.github.com>
2022-11-24 11:52:59 -0500

    Update requestHandlers.js

--- requestHandlers.js
+++ requestHandlers.js
@@ -21,7 +21,7 @@ function start(response) {
         'var radius = 70;'+
         'context.beginPath();'+
         'context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);'+
-        'context.fillStyle = "green";'+
+        'context.fillStyle = "#b6c406";'+
         'context.fill();'+
         'context.lineWidth = 5;'+
         'context.strokeStyle = "#003300";'+
